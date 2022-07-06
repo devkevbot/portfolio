9fd0f47016b1737276901bac8c64d5a5d9b80794
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2022-07-06 13:36:24 -0700

    feat: update navbar with internal links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,6 @@
-import MediaLinks from "./media-links";
-
 const Footer = () => (
-  <footer className="flex flex-col items-center gap-8 border-t-2 border-white bg-slate-900 p-4 font-semibold uppercase text-white">
-    <MediaLinks />
-    <span className="normal-case">
-      Made by Kevin Jerome &mdash; {new Date().getFullYear()}
-    </span>
+  <footer className="flex flex-col items-center gap-8 border-t-2 border-white bg-slate-900 p-4 font-semibold text-white">
+    <span>Made by Kevin Jerome &mdash; {new Date().getFullYear()}</span>
   </footer>
 );
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,56 @@
-import MediaLinks from "./media-links";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const internalLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
+
+const internalLinkIsActive = (activePathname: string, targetHref: string) =>
+  activePathname === targetHref;
+
+const InternalLinks = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <ul className="flex justify-center gap-16">
+      {internalLinks.map(link => (
+        <li key={link.name}>
+          <Link href={link.href}>
+            <button
+              className={`rounded-2xl border-2 px-4 py-1 hover:border-sky-300 hover:text-sky-300 ${
+                internalLinkIsActive(pathname, link.href)
+                  ? "border-sky-400 text-sky-400"
+                  : "border-white text-white"
+              }`}
+              type="button"
+            >
+              {link.name}
+            </button>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Navbar = () => (
   <nav className="border-b-2 border-white bg-slate-900 p-4 font-semibold uppercase text-white">
-    <MediaLinks />
+    <InternalLinks />
   </nav>
 );
 
