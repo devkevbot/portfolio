8e5ab8b0836539ecf6ab1a34c1844b58a0bf2667
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2022-07-06 01:09:08 -0700

    feat: simple navbar styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,26 @@
-const Navbar = () => <nav className="bg-slate-400">Hello from navbar</nav>;
+const navLinks = [
+  {
+    name: "Repositories",
+    href: "https://github.com/devkevbot?tab=repositories",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/feed/",
+  },
+];
+
+const Navbar = () => (
+  <nav className="border-b-2 border-white bg-slate-900 p-4 font-semibold uppercase text-white">
+    <ul className="flex justify-center gap-16">
+      {navLinks.map(link => (
+        <li key={link.name}>
+          <a href={link.href} target="_blank" rel="noreferrer">
+            {link.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Navbar;
