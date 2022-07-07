f958fb5449b6c30c3b09f7d9b1fe096fe12b7491
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2022-07-07 15:14:43 -0700

    fix: scroll to top on route transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Router from "next/router";
 import Layout from "../components/layout";
 import Metatags from "../components/metatags";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on("routeChangeComplete", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
   return (
     <>
       <Metatags />
