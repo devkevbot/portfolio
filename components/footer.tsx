79f0c141ce833d553a726b5840f886b7208edd05
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2022-07-06 21:49:22 -0700

    fix: remove footer border

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="flex flex-col items-center gap-8 border-t-2 border-white bg-slate-900 p-4 font-semibold text-white">
+  <footer className="flex flex-col items-center gap-8 bg-slate-900 p-4 font-semibold text-white">
     <span>Made by Kevin Jerome &mdash; {new Date().getFullYear()}</span>
   </footer>
 );
